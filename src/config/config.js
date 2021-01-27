c6090608c6fcba6728efb850ae16f3eea179bad6
Thigo <thiago.l17@yahoo.com>
2021-01-27 02:07:40 -0300

    adding head

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ export const MY_SEO = {
   description: 'Quiz NodeJS - Imersão React Nextjs',
   openGraph: {
       type: 'website',
-      url: 'My URL',
+      url: 'https://project-quiz.thilourenco.vercel.app/',
       title: 'Quiz NodeJS',
       description: 'Quiz NodeJS - Imersão React Nextjs',
       image: 'https://cdn.freebiesupply.com/logos/thumbs/2x/nodejs-1-logo.png', // credits: https://freebiesupply.com/logos/node-js-logo/
