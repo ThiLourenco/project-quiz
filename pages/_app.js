93118c2da1ca15dd6631094f42d5919fea95a2a0
Thigo <thiago.l17@yahoo.com>
2021-01-27 04:06:03 -0300

    img

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import db from '../db.json';
-import Head from '../src/components/Head';
+//import Head from '../src/components/Head';
+import Head from 'next/head';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -30,7 +31,22 @@ const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
-    <><Head />
+    <>
+      <Head>
+      <meta key="og:image" property="og:image" content={MY_SEO.openGraph.image} />
+      <meta key="og:image:secure_url" property="og:image:secure_url" content={MY_SEO.openGraph.image} />
+      <meta key="og:image:alt" property="og:image:alt" content="NodeJS"/>
+      <meta key="og:image:type" property="og:image:type" content="image/jpg"/>
+      <meta key="og:image:width" property="og:image:width" content="1200"/>
+      <meta key="og:image:height" property="og:image:height" content="630"/>
+      <meta key="twitter:card" name="twitter:card" content="summary_large_image"/>
+      <meta key="twitter:title" name="twitter:title" content="Quiz Nodejs"/>
+      <meta Key="twitter:site" name="twitter:site" content="@thilourenco"/>
+      <meta Key="twitter:creator" name="twitter:creator" content="@thilourenco"/>
+      <meta Key="twitter:image" name="twitter:image" content={MY_SEO.openGraph.image} />
+      <meta Key="twitter:image:src" name="twitter:image:src" content={MY_SEO.openGraph.image} />
+      <meta Key="twitter:image:alt" name="twitter:image:alt" content="NodeJS"/>    
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
