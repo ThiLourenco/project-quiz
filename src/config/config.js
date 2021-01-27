4ea556084687fdb7650c14cf27260de45bb1dd8a
Thigo <thiago.l17@yahoo.com>
2021-01-27 03:30:07 -0300

    adding head

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,7 @@ export const MY_SEO = {
       title: 'Quiz NodeJS',
       description: 'Quiz NodeJS - Imersão React Nextjs',
       keywords: 'Quiz NodeJS - Imersão React NextJs',
-      image: 'https://grozav.com/content/images/2020/04/green-forest-road.jpg', // credits: https://nodejs.org/en/
+      image: 'https://grozav.com/content/images/2020/04/green-forest-road.jpg',
+
   }
 };
