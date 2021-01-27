7821b34e6eb61f996a90ebae6074d8a05499bb13
Thigo <thiago.l17@yahoo.com>
2021-01-27 02:03:10 -0300

    adding head

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import db from '../db.json';
+import Meta from '../src/components/Head';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -29,7 +30,7 @@ const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <><Meta />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+export const MY_SEO = {
+  title: 'NodeJS Quiz',
+  description: 'Quiz NodeJS - Imersão React Nextjs',
+  openGraph: {
+      type: 'website',
+      url: 'My URL',
+      title: 'Quiz NodeJS',
+      description: 'Quiz NodeJS - Imersão React Nextjs',
+      image: 'https://cdn.freebiesupply.com/logos/thumbs/2x/nodejs-1-logo.png', // credits: https://freebiesupply.com/logos/node-js-logo/
+  }
+};
