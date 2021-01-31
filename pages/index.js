95a7175f8b28fcd159284c4d4a4a07d7d0c8dba7
Thigo <thiago.l17@yahoo.com>
2021-01-31 09:10:14 -0300

    adding quiz container and form

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
+// eslint-disable-next-line import/no-named-as-default
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
-// eslint-disable-next-line import/no-named-as-default
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubConer from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // const BackgroundImage = styled.div `
 //   background-image: url(${db.bg});
@@ -15,17 +17,6 @@ import GitHubConer from '../src/components/GitHubCorner';
 //   background-size: cover;
 //   background-position: center;
 // `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -46,24 +37,17 @@ export default function Home() {
         <form onSubmit={function (e) {
           e.preventDefault();
           router.push(`/quiz?name=${name}`);
-          console.log('fazendo uma submissão com react');
         }}
         >
-          <input
-            onChange={function (e) {
-              // state
-              // name = e.target.value;
-              setName(e.target.value);
-            }}
+          <Input
+            name="NomeDoCampo"
+            onChange={(e) => setName(e.target.value)}
             placeholder="Informe o seu nome"
+            value={name}
           />
-          <button
-            type="submit"
-            disabled={name.length === 0}
-          >
-            Jogar
-            {name}
-          </button>
+          <Button type="submit" disabled={name.length <= 0}>
+            {`Jogar ${name}`}
+          </Button>
         </form>
         <Widget>
           <Widget.Header>
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -7,14 +7,12 @@ const Widget = styled.div`
   background-color: ${({ theme }) => theme.colors.mainBg};
   border-radius: 4px;
   overflow: hidden;
-
   h1, h2, h3 {
     font-size: 16px;
     font-weight: 700;
     line-height: 1;
     margin-bottom: 0;
   }
-
   p {
     font-size: 14px;
     font-weight: 400;
@@ -28,11 +26,10 @@ Widget.Header = styled.header`
   align-items: center;
   padding: 18px 32px;
   background-color: ${({ theme }) => theme.colors.primary};
-
+  
   * {
     margin: 0;
   }
-
 `;
 
 Widget.Content = styled.div`
@@ -40,14 +37,30 @@ Widget.Content = styled.div`
   & > *:first-child {
     margin-top: 0;
   }
-
-  & > *:first-child {
+  & > *:last-child {
     margin-bottom: 0;
   }
-
   ul {
     list-style: none;
     padding: 0;
+  }
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: .3s;
+  display: block;
+  
+  &:hover,
+  &:focus {
+    opacity: .5;
   }
 `;
 
